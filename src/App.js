5b776d809e0e2dad5e9544295a62c620ecb8f702
Dean Mayne <deancmayne@gmail.com>
2021-01-15 14:48:51 -0800

    add router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import FourOhFour from "./components/fourohfour";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <FourOhFour/>
+      <Switch>
+        <Route exact path="/" component={FourOhFour} />
+        <Redirect to="/" />
+      </Switch>
       <Footer />
     </div>
   );
