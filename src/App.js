245d2a4ec4194a9d84146871a705219b76abe510
Dean Mayne <deancmayne@gmail.com>
2021-01-14 23:18:54 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Navbar} from './components/navbar';
+
+
+const App = () => {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/application.scss';
+
+export const Navbar = () => (
+
+    <div className="navbar">
+        <label>Dean Mayne</label>
+        <a href="http://www.linkedin.com/in/deanmayne">Linkedin</a>
+        <a href="http://www.github.com/in/deanmayne">Github</a>
+        
+    </div>
+)
