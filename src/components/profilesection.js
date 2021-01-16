76ffafec932eed66f6eae9ff1789fdfe8fe21fdb
Dean Mayne <deancmayne@gmail.com>
2021-01-15 21:58:28 -0800

    css

--- src/components/profilesection.js
+++ src/components/profilesection.js
@@ -4,11 +4,13 @@ const ProfileSection = () => (
   <div className="profile">
     <img className="profile__headshot" src="./Headshot.jpg" alt="headshot" />
     <p className="profile__bio">
-      Hi I'm Dean ! I am a newly minted Software Engineer who spent a lot of
-      time in my past life in working in the fashion industry. Now I'm on the
-      hunt for a backend or full-stack job ! Technologies and skills I have in
-      my toolbelt include Javascript, Ruby on Rails, React/Redux, the MERN
-      Stack, R, and many others you can find on my{" "}
+      Hi I'm <span>Dean</span> ! I am a newly minted{" "}
+      <span>Software Engineer</span> who spent a lot of time in my past life in
+      working in the fashion industry. Now I'm on the hunt for a backend or
+      full-stack job ! Technologies and skills I have in my toolbelt include{" "}
+      <span>Javascript</span>, <span>Ruby on Rails</span>,{" "}
+      <span>React/Redux</span>, the <span>MERN</span> Stack, <span>R</span>, and
+      many others you can find on my{" "}
       <a href="http://www.linkedin.com/in/deanmayne">LinkedIn</a> or{" "}
       <a href="http://www.github.com/deanmayne">Github</a>, along with my past
       and current projects !
